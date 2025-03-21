06e670e07855573dc8ad4ef4155c664f252e26ea
Halyna Moldovianu <Beorgeos@gmail.com>
2025-03-21 13:22:49 +0100

    3

--- src/js/projects.js
+++ src/js/projects.js
@@ -1,42 +1,9 @@
 import projects from '../json/projects.json';
+import { projectsTemplate } from './render-functions';
 // searching for the elements
 const refs = {
   projectsList: document.querySelector('.js-project-cards-list'),
   loadMoreBtn: document.querySelector('.js-load-more-btn'),
-};
-
-// function creating the projects section template
-const projectsTemplate = projects => {
-  return projects
-    .map(project => {
-      const { title, url1, url2, technologies } = project;
-      return `
-      <li class="project-card">
-        <picture>
-          <source
-            srcset="${url1} 1x, ${url2} 2x"
-          />
-          <img
-            class="my-project-picture"
-            src="${url2} 2x"
-            alt="${title}"
-            loading="lazy"
-          />
-        </picture>
-        <p class="technologies-list">${technologies}</p>
-        <h3 class="project-description">${title}</h3>
-        <div class="link-container">
-          <a class="visit-link" href="#">
-            visit
-            <svg class="visit-arrow" width="14" height="14">
-              <use href="../img/icons.svg#visit-arrow"></use>
-            </svg>
-          </a>
-        </div>
-      </li>
-    `;
-    })
-    .join('');
 };
 
 let startIndex = 0;
@@ -60,6 +27,8 @@ const onLoadMoreBtn = () => {
   refs.projectsList.insertAdjacentHTML('beforeend', projectTemplate);
   if (endIndex >= projects.length) {
     refs.loadMoreBtn.classList.add('visually-hidden');
+    refs.loadMoreBtn.removeEventListener('click', onLoadMoreBtn);
+    return;
   }
   setTimeout(() => {
     const height = refs.projectsList.getBoundingClientRect().height;
