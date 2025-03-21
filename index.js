fd4abe374d1c13bc3114a0f3ff374d9dc551ea95
Vladyslav-Avrashov <vladisloveavrashov@gmail.com>
2025-03-21 12:50:22 +0000

    Deploying to gh-pages from @ Vladyslav-Avrashov/project_js_1@3d622a3d74aaadad80b20611bfe14919b603ddf6 🚀

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
-(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const o of r.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&i(o)}).observe(document,{childList:!0,subtree:!0});function s(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function i(e){if(e.ep)return;e.ep=!0;const r=s(e);fetch(e.href,r)}})();
+(function(){const i=document.createElement("link").relList;if(i&&i.supports&&i.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))c(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const n of t.addedNodes)n.tagName==="LINK"&&n.rel==="modulepreload"&&c(n)}).observe(document,{childList:!0,subtree:!0});function o(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function c(e){if(e.ep)return;e.ep=!0;const t=o(e);fetch(e.href,t)}})();const l=[{title:"Wallet webservice",url1:"./img/projectimages/manage-finances@1x.jpg",url2:"./img/projectimages/manage-finances@2x.jpg",technologies:"React, JavaScript, Node JS, Git"},{title:"Green harvest webservice",url1:"./img/projectimages/organic-vegetables@1x.jpg",url2:"./img/projectimages/organic-vegetables@2x.jpg",technologies:"React, JavaScript, Node JS, Git"},{title:"English Exellence webservice",url1:"./img/projectimages/discover@1x.jpg",url2:"./img/projectimages/discover@2x.jpg",technologies:"React, JavaScript, Node JS, Git"},{title:"Power pulse webservice",url1:"./img/projectimages/transforming@1x.jpg",url2:"./img/projectimages/transforming@2x.jpg",technologies:"React, JavaScript, Node JS, Git"},{title:"Mimino website",url1:"./img/projectimages/mimino@@1x.jpg",url2:"./img/projectimages/mimino@2x.jpg",technologies:"React, JavaScript, Node JS, Git"},{title:"Vyshyvanka vibes Landing Page",url1:"./img/projectimages/reviving-traditions@1x.jpg",url2:"./img/projectimages/reviving-traditions@2x.jpg",technologies:"React, JavaScript, Node JS, Git"},{title:"Chego jewelry website",url1:"./img/projectimages/a-brand@1x.jpg",url2:"./img/projectimages/a-brand@2x.jpg",technologies:"React, JavaScript, Node JS, Git"},{title:"Energy flow webservice",url1:"./img/projectimages/bodyshape@1x.jpg",url2:"./img/projectimages/bodyshape@2x.jpg",technologies:"React, JavaScript, Node JS, Git"},{title:"fruitbox online store",url1:"./img/projectimages/fresh-harvest@1x.jpg",url2:"./img/projectimages/fresh-harvest@2x.jpg",technologies:"React, JavaScript, Node JS, Git"},{title:"Starlight studio landing page",url1:"./img/projectimages/turn-on@1x.jpg",url2:"./img/projectimages/turn-pn@2x.jpg",technologies:"React, JavaScript, Node JS, Git"}],p=r=>r.map(i=>{const{title:o,url1:c,url2:e,technologies:t}=i;return`
+      <li class="project-card">
+        <picture>
+          <source
+            srcset="${c} 1x, ${e} 2x"
+          />
+          <img
+            class="my-project-picture"
+            src="${e} 2x"
+            alt="${o}"
+            loading="lazy"
+          />
+        </picture>
+        <p class="technologies-list">${t}</p>
+        <h3 class="project-description">${o}</h3>
+        <div class="link-container">
+          <a class="visit-link" href="#">
+            visit
+            <svg class="visit-arrow" width="14" height="14">
+              <use href="../img/icons.svg#visit-arrow"></use>
+            </svg>
+          </a>
+        </div>
+      </li>
+    `}).join(""),s={projectsList:document.querySelector(".js-project-cards-list"),loadMoreBtn:document.querySelector(".js-load-more-btn")};let g=0,a=3;const d=()=>{const r=l.slice(g,a),i=p(r);s.projectsList.innerHTML=i};d();const m=()=>{g+=3,a+=3;const r=l.slice(g,a),i=p(r);if(s.projectsList.insertAdjacentHTML("beforeend",i),setTimeout(()=>{const o=s.projectsList.getBoundingClientRect().height;window.scrollBy({top:o*10,behavior:"smooth"})},0),a>=l.length){s.loadMoreBtn.classList.add("visually-hidden"),s.loadMoreBtn.removeEventListener("click",m);return}};s.loadMoreBtn.addEventListener("click",m);
 //# sourceMappingURL=index.js.map
