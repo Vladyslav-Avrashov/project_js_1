76dbad722995377bc69863f94c4adedd6d179e83
Halyna Moldovianu <Beorgeos@gmail.com>
2025-03-24 15:29:08 +0100

    6

--- src/js/projects-data.js
+++ src/js/projects-data.js
@@ -3,6 +3,31 @@ import wallet2 from '../img/projectimages/manage-finances@2x.jpg';
 
 import organic1 from '../img/projectimages/organic-vegetables@1x.jpg';
 import organic2 from '../img/projectimages/organic-vegetables@2x.jpg';
+
+import discover1 from '../img/projectimages/discover@1x.jpg';
+import discover2 from '../img/projectimages/discover@2x.jpg';
+
+import transforming1 from '../img/projectimages/transforming@1x.jpg';
+import transforming2 from '../img/projectimages/transforming@2x.jpg';
+
+import mimino1 from '../img/projectimages/mimino@@1x.jpg';
+import mimino2 from '../img/projectimages/mimino@2x.jpg';
+
+import traditions1 from '../img/projectimages/reviving-traditions@1x.jpg';
+import traditions2 from '../img/projectimages/reviving-traditions@2x.jpg';
+
+import brand1 from '../img/projectimages/a-brand@1x.jpg';
+import brand2 from '../img/projectimages/a-brand@2x.jpg';
+
+import bodyshape1 from '../img/projectimages/bodyshape@1x.jpg';
+import bodyshape2 from '../img/projectimages/bodyshape@2x.jpg';
+
+import harvest1 from '../img/projectimages/fresh-harvest@1x.jpg';
+import harvest2 from '../img/projectimages/fresh-harvest@2x.jpg';
+
+import turn1 from '../img/projectimages/turn-on@1x.jpg';
+import turn2 from '../img/projectimages/turn-pn@2x.jpg';
+
 export const projectData = [
   {
     title: 'Wallet webservice',
@@ -18,50 +43,50 @@ export const projectData = [
   },
   {
     title: 'English Exellence webservice',
-    url1: './img/projectimages/discover@1x.jpg',
-    url2: './img/projectimages/discover@2x.jpg',
+    url1: discover1,
+    url2: discover2,
     technologies: 'React, JavaScript, Node JS, Git',
   },
   {
     title: 'Power pulse webservice',
-    url1: './img/projectimages/transforming@1x.jpg',
-    url2: './img/projectimages/transforming@2x.jpg',
+    url1: transforming1,
+    url2: transforming2,
     technologies: 'React, JavaScript, Node JS, Git',
   },
   {
     title: 'Mimino website',
-    url1: './img/projectimages/mimino@@1x.jpg',
-    url2: './img/projectimages/mimino@2x.jpg',
+    url1: mimino1,
+    url2: mimino2,
     technologies: 'React, JavaScript, Node JS, Git',
   },
   {
     title: 'Vyshyvanka vibes Landing Page',
-    url1: './img/projectimages/reviving-traditions@1x.jpg',
-    url2: './img/projectimages/reviving-traditions@2x.jpg',
+    url1: traditions1,
+    url2: traditions2,
     technologies: 'React, JavaScript, Node JS, Git',
   },
   {
     title: 'Chego jewelry website',
-    url1: './img/projectimages/a-brand@1x.jpg',
-    url2: './img/projectimages/a-brand@2x.jpg',
+    url1: brand1,
+    url2: brand2,
     technologies: 'React, JavaScript, Node JS, Git',
   },
   {
     title: 'Energy flow webservice',
-    url1: './img/projectimages/bodyshape@1x.jpg',
-    url2: './img/projectimages/bodyshape@2x.jpg',
+    url1: bodyshape1,
+    url2: bodyshape2,
     technologies: 'React, JavaScript, Node JS, Git',
   },
   {
     title: 'fruitbox online store',
-    url1: './img/projectimages/fresh-harvest@1x.jpg',
-    url2: './img/projectimages/fresh-harvest@2x.jpg',
+    url1: harvest1,
+    url2: harvest2,
     technologies: 'React, JavaScript, Node JS, Git',
   },
   {
     title: 'Starlight studio landing page',
-    url1: './img/projectimages/turn-on@1x.jpg',
-    url2: './img/projectimages/turn-pn@2x.jpg',
+    url1: turn1,
+    url2: turn2,
     technologies: 'React, JavaScript, Node JS, Git',
   },
 ];
